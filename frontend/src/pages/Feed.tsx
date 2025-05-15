0a654d3fe1f787ae23d1a201d3a9ddd86af526b8
RitickLath <ritick943lath@gmail.com>
2025-05-15 16:56:37 +0530

    Drag to chanage the profile | left and right grag is captured

--- frontend/src/pages/Feed.tsx
+++ frontend/src/pages/Feed.tsx
@@ -3,17 +3,19 @@ import { motion } from "framer-motion";
 import { imagesResponse } from "../constants/hardcoded/constants";
 
 const Feed = () => {
-  const [index] = useState<number>(0);
+  const [index, setIndex] = useState<number>(0);
   const [innerIndex, setInnerIndex] = useState<number>(0);
-
-  const handleTap = () => {
+  
+  const handleNext = () => {
     setInnerIndex((prev) => (prev + 1) % imagesResponse[index].images.length);
   };
-  // ON Swipe we will set the innerIndex to 0 and increment the index by one in all cases we dont allow previous for index value
 
   return (
-    <div className="p-4 bg-[#111418] text-white w-full h-[80dvh] flex flex-col">
-      {/* Image view progress bar */}
+    <div
+      onClick={handleNext}
+      className="p-4 bg-[#111418] text-white w-full h-[80dvh] flex flex-col"
+    >
+      {/* Progress Bar */}
       <div className="flex gap-2 h-1 mb-4">
         {imagesResponse[index].images.map((_, ind) => (
           <div
@@ -25,12 +27,22 @@ const Feed = () => {
         ))}
       </div>
 
-      {/* Image Viewer */}
-      <div
-        onClick={handleTap}
-        className="flex-1 w-full flex justify-center items-center cursor-pointer relative overflow-hidden rounded-xl"
-      >
+      {/* Image Viewer with Swipe */}
+      <div className="flex-1 w-full flex justify-center items-center cursor-pointer relative overflow-hidden rounded-xl">
         <motion.img
+          drag
+          onDragEnd={(_, info) => {
+            if (info.offset.x > 0) {
+              console.log("Right");
+              setIndex((prev) => (prev + 1) % imagesResponse.length);
+              setInnerIndex(0);
+            } else {
+              console.log("Left");
+              setIndex((prev) => (prev + 1) % imagesResponse.length);
+              setInnerIndex(0);
+            }
+          }}
+          dragConstraints={{ right: 100, left: -100, top: 4, bottom: 4 }}
           key={imagesResponse[index].images[innerIndex]}
           src={imagesResponse[index].images[innerIndex]}
           alt={`Slide ${innerIndex + 1}`}
