c5217115a3de9bae5e3d348af689ac26ec93145b
RitickLath <ritick943lath@gmail.com>
2025-05-20 18:39:24 +0530

    Horizontal Overscroll + Logo Color

--- frontend/src/component/Navbar.tsx
+++ frontend/src/component/Navbar.tsx
@@ -1,9 +1,13 @@
 const Navbar = () => {
   return (
     <div className="text-white h-[10dvh] w-full flex items-center justify-between px-6 py-4">
-      <h1 className="text-2xl font-bold">tinder</h1>
+      <h1 className="text-3xl font-bold bg-gradient-to-r from-amber-200 to-pink-600 bg-clip-text text-transparent">
+        tinder
+      </h1>
     </div>
   );
 };
 
 export default Navbar;
+
+// text-[#FF4458]
